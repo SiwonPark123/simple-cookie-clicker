1469154484ae4cfd61e35ff323bc09274ae7e2a8
Siwon Park <890186par@ames.k12.ia.us>
2026-09-02 20:45:35 -0500

    made a way to save game data locally

--- cookie.js
+++ cookie.js
@@ -1,64 +1,69 @@
-var cookieAmount = 0;
-var upgrade1Cost = 50;
-var upgrade2Cost = 500;
-var upgrade2Amount = 0;
-var upgrade3Cost = 2500;
-var upgrade3Amount = 0;
 var upgradeMult = 1.25;
-var clickAmount = 1;
-var cookiesPerSecond = 0;
 var addCPS;
 var notEnoughCookiesTimer;
-
-const gameState = {
-    cookies: 0,
-    upgrade1Amount: 0,
-    upgrade2Amount: 0,
-    upgrade3Amount: 0,
+var gameState = {
+    cookieAmount: 0,
     upgrade1Cost: 50,
+    upgrade2Amount: 0,
     upgrade2Cost: 500,
+    upgrade3Amount: 0,
     upgrade3Cost: 2500,
     clickAmount: 1,
     cookiesPerSecond: 0
 }
 
+loadGameState();
+
+function loadGameState() {
+    var storedState = localStorage.getItem('gameState');
+    if (storedState !== null) {
+        gameState = JSON.parse(storedState);
+    }
+    document.getElementById("cookieAmount").textContent = "Cookie Amount: " + gameState.cookieAmount;
+    document.getElementById("cookiesPerSecond").textContent = "Cookies per Second: " + gameState.cookiesPerSecond;
+    document.getElementById("clickAmount").textContent = "Cookies per Click: " + gameState.clickAmount;
+    document.getElementById("upgrade1Cost").textContent = "Cost: " + gameState.upgrade1Cost + "\u2003";
+    document.getElementById("upgrade2Cost").textContent = "Cost: " + gameState.upgrade2Cost + "\u2003";
+    document.getElementById("upgrade3Cost").textContent = "Cost: " + gameState.upgrade3Cost + "\u2003";
+}
+
 function clickCookie() {
-    cookieAmount += clickAmount;
-    document.getElementById("cookieAmount").textContent = "Cookie Amount: " + cookieAmount;
+    gameState.cookieAmount += gameState.clickAmount;
+    document.getElementById("cookieAmount").textContent = "Cookie Amount: " + gameState.cookieAmount;
 }
 
 function calculateCookiesPerSecond() {
     clearInterval(addCPS);
-    var upgrade2CPS = upgrade2Amount * 10;
-    var upgrade3CPS = upgrade3Amount * 30;
-    cookiesPerSecond = upgrade2CPS + upgrade3CPS;
-    document.getElementById("cookiesPerSecond").textContent = "Cookies per Second: " + cookiesPerSecond;
+    var upgrade2CPS = gameState.upgrade2Amount * 10;
+    var upgrade3CPS = gameState.upgrade3Amount * 30;
+    gameState.cookiesPerSecond = upgrade2CPS + upgrade3CPS;
+    document.getElementById("cookiesPerSecond").textContent = "Cookies per Second: " + gameState.cookiesPerSecond;
     addCPS = setInterval(() => {
-        cookieAmount += cookiesPerSecond;
-        document.getElementById("cookieAmount").textContent = "Cookie Amount: " + cookieAmount;
+        gameState.cookieAmount += gameState.cookiesPerSecond;
+        document.getElementById("cookieAmount").textContent = "Cookie Amount: " + gameState.cookieAmount;
     }, 1000);
 }
 
 function upgrade1() {
-    if (cookieAmount >= upgrade1Cost) {
-        cookieAmount -= upgrade1Cost;
-        document.getElementById("cookieAmount").textContent = "Cookie Amount: " + cookieAmount;
-        clickAmount++;
-        document.getElementById("clickAmount").textContent = "Cookies per Click: " + clickAmount;
-        upgrade1Cost = Math.floor(upgrade1Cost * upgradeMult);
-        document.getElementById("upgrade1Cost").textContent = "Cost: " + upgrade1Cost + "\u2003";
+    if (gameState.cookieAmount >= gameState.upgrade1Cost) {
+        gameState.cookieAmount -= gameState.upgrade1Cost;
+        document.getElementById("cookieAmount").textContent = "Cookie Amount: " + gameState.cookieAmount;
+        gameState.clickAmount++;
+        document.getElementById("clickAmount").textContent = "Cookies per Click: " + gameState.clickAmount;
+        gameState.upgrade1Cost = Math.floor(gameState.upgrade1Cost * upgradeMult);
+        document.getElementById("upgrade1Cost").textContent = "Cost: " + gameState.upgrade1Cost + "\u2003";
     } else {
         notEnoughCookies("upgrade1");
     }
 }
 
 function upgrade2() {
-    if (cookieAmount >= upgrade2Cost) {
-        upgrade2Amount++;
-        cookieAmount -= upgrade2Cost;
-        document.getElementById("cookieAmount").textContent = "Cookie Amount: " + cookieAmount;
-        upgrade2Cost = Math.floor(upgrade2Cost * upgradeMult);
-        document.getElementById("upgrade2Cost").textContent = "Cost: " + upgrade2Cost + "\u2003";
+    if (gameState.cookieAmount >= gameState.upgrade2Cost) {
+        gameState.upgrade2Amount++;
+        gameState.cookieAmount -= gameState.upgrade2Cost;
+        document.getElementById("cookieAmount").textContent = "Cookie Amount: " + gameState.cookieAmount;
+        gameState.upgrade2Cost = Math.floor(gameState.upgrade2Cost * upgradeMult);
+        document.getElementById("upgrade2Cost").textContent = "Cost: " + gameState.upgrade2Cost + "\u2003";
         calculateCookiesPerSecond();
     } else {
         notEnoughCookies("upgrade2");
@@ -66,12 +71,12 @@ function upgrade2() {
 }
 
 function upgrade3() {
-    if (cookieAmount >= upgrade3Cost) {
-        upgrade3Amount++;
-        cookieAmount -= upgrade3Cost;
-        document.getElementById("cookieAmount").textContent = "Cookie Amount: " + cookieAmount;
-        upgrade3Cost = Math.floor(upgrade3Cost * upgradeMult);
-        document.getElementById("upgrade3Cost").textContent = "Cost: " + upgrade3Cost + "\u2003";
+    if (gameState.cookieAmount >= gameState.upgrade3Cost) {
+        gameState.upgrade3Amount++;
+        gameState.cookieAmount -= gameState.upgrade3Cost;
+        document.getElementById("cookieAmount").textContent = "Cookie Amount: " + gameState.cookieAmount;
+        gameState.upgrade3Cost = Math.floor(gameState.upgrade3Cost * upgradeMult);
+        document.getElementById("upgrade3Cost").textContent = "Cost: " + gameState.upgrade3Cost + "\u2003";
         calculateCookiesPerSecond();
     } else {
         notEnoughCookies("upgrade3");
@@ -84,3 +89,10 @@ function notEnoughCookies(id) {
         document.getElementById(id).style.borderColor = "rgb(136, 136, 116)";
     }, 1500);
 }
+
+function update() {
+    localStorage.setItem('gameState', JSON.stringify(gameState));
+    requestAnimationFrame(update);
+}
+
+update()
